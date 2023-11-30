049b138bd13da88371b3f0b167b2eb9648941e95
junan76 <junan76@163.com>
2023-11-30 22:28:06 +0800

    leetcode: 对称二叉树的判断.
    思路:
    使用递归的方法进行比较.
    
    Signed-off-by: junan76 <junan76@163.com>

---
+++ src/symmetric_tree.cpp
@@ -0,0 +1,30 @@
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isSymmetric(TreeNode *root) {
+    if (root == nullptr) return true;
+
+    return compareTrees(root->left, root->right);
+  }
+
+ private:
+  bool compareTrees(const TreeNode *left, const TreeNode *right) {
+    if (left == right && left == nullptr) return true;
+
+    if ((left != nullptr && right == nullptr) ||
+        (left == nullptr && right != nullptr) || left->val != right->val)
+      return false;
+
+    return compareTrees(left->left, right->right) &&
+           compareTrees(left->right, right->left);
+  }
+};
