0fc5b3ed1b38d701172f968324393ce2791091db
junan <junan76@163.com>
2024-07-25 11:14:39 +0800

    [3sum]: 排序之后, 使用双指针, 将循环降为两层.
    注意去重问题.
    
    Signed-off-by: junan <junan76@163.com>

--- src/three_sum.cpp
+++ src/three_sum.cpp
@@ -12,7 +12,7 @@ using std::vector;
 /**
  * 三重循环, 暴力求解.
  */
-class Solution {
+class Solution1 {
 public:
   vector<vector<int>> threeSum(vector<int> &nums) {
     vector<vector<int>> results;
@@ -60,6 +60,55 @@ public:
 
       answer.pop_back();
       sum -= nums[i];
+    }
+
+    return results;
+  }
+};
+
+/**
+ * 排序之后使用双指针, 复杂度降为O(n^2)
+ */
+class Solution {
+public:
+  vector<vector<int>> threeSum(vector<int> &nums) {
+    vector<vector<int>> results;
+    vector<int> answer;
+    std::sort(nums.begin(), nums.end());
+
+    int i = 0, j, k;
+    int n = nums.size();
+
+    for (; i < n; i++) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+
+      answer.push_back(nums[i]);
+      j = i + 1;
+      k = n - 1;
+
+      while (j < k) {
+        if (nums[j] + nums[k] > -nums[i])
+          k--;
+        else if ((nums[j] + nums[k] < -nums[i]))
+          j++;
+        else {
+          answer.push_back(nums[j]);
+          answer.push_back(nums[k]);
+          results.push_back(vector<int>(answer));
+          answer.pop_back();
+          answer.pop_back();
+          j++;
+          k--;
+
+          while (j < k && nums[j] == nums[j - 1])
+            j++;
+          while (j < k && nums[k] == nums[k + 1])
+            k--;
+        }
+      }
+
+      answer.pop_back();
     }
 
     return results;
